8800daa1dd2733ae23ad8520e3105bffa27280e6
Facundo Frau <facufrau@gmail.com>
2023-06-10 12:05:24 -0300

    Complete Problem Set 0

---
+++ ps0/pset0.py
@@ -0,0 +1,6 @@
+from math import log2
+
+number_x = int(input("Enter number x: "))
+number_y = int(input("Enter number y: "))
+print(f"x**y = {number_x ** number_y}")
+print(f"log2(x) = {log2(number_x)}")
